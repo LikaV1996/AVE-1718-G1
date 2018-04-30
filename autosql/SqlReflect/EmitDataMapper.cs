1c90fdeb77df6fbc79e0c36d90e574b1aab5f962
LikaV1996 <lcvasko@gmail.com>
2018-04-30 15:44:43 +0100

    EmitDataMapper changes (not buildable)

--- autosql/SqlReflect/EmitDataMapper.cs
+++ autosql/SqlReflect/EmitDataMapper.cs
@@ -32,11 +32,11 @@ namespace SqlReflect
                     TypeAttributes.Public,
                     typeof(DynamicDataMapper)
                 );
-            
+
 
             // Define a constructor that takes same arguments of DynamicDataMapper. 
             Type[] parameterTypes = { typeof(Type), typeof(string), typeof(bool) };
-            ConstructorBuilder ctor = 
+            ConstructorBuilder ctor =
                 tb.DefineConstructor(
                     MethodAttributes.Public,
                     CallingConventions.Standard,
@@ -50,17 +50,17 @@ namespace SqlReflect
             il.Emit(OpCodes.Ret);
 
             // Overrides AbstractDataMapper methods
-            
+
             Type[] parametersType;
 
-            parametersType = new Type[]{ typeof(IDataReader) };
-            MethodBuilder mbLoadMethod = buildMethod(tb, "Load", klass, parametersType);
-                
-            parametersType = new Type[]{ typeof(object) };
-            MethodBuilder mbSQLInsertMethod = buildMethod(tb, "sqlInsert", typeof(string), parametersType);   
-            MethodBuilder mbSQLDeleteMethod = buildMethod(tb, "sqlDelete", typeof(string), parametersType);  
+            parametersType = new Type[] { typeof(IDataReader) };
+            MethodBuilder mbLoadMethod = BuildLoadMethod(tb, "Load", klass);
+
+            parametersType = new Type[] { typeof(object) };
+            MethodBuilder mbSQLInsertMethod = buildMethod(tb, "sqlInsert", typeof(string), parametersType);
+            MethodBuilder mbSQLDeleteMethod = buildMethod(tb, "sqlDelete", typeof(string), parametersType);
             MethodBuilder mbSQLUpdateMethod = buildMethod(tb, "sqlUpdate", typeof(string), parametersType);
-                
+
             Type type = tb.CreateType();
 
             DynamicDataMapper ddm = (DynamicDataMapper)Activator.CreateInstance(type);
@@ -68,68 +68,7 @@ namespace SqlReflect
             ab.Save(aName.Name + ".dll");
 
             return ddm;
-            /*
-            AssemblyName aName = new AssemblyName("DynamicAssemblyExample");
-        AssemblyBuilder ab = 
-            AppDomain.CurrentDomain.DefineDynamicAssembly(
-                aName, 
-                AssemblyBuilderAccess.RunAndSave);
 
-        // For a single-module assembly, the module name is usually
-        // the assembly name plus an extension.
-        ModuleBuilder mb = 
-            ab.DefineDynamicModule(aName.Name, aName.Name + ".dll");
-
-        TypeBuilder tb = mb.DefineType(
-            "MyDynamicType", 
-             TypeAttributes.Public);
-
-        // Add a private field of type int (Int32).
-        FieldBuilder fbNumber = tb.DefineField(
-            "m_number", 
-            typeof(int), 
-            FieldAttributes.Private);
-
-        // Define a constructor that takes an integer argument and 
-        // stores it in the private field. 
-        Type[] parameterTypes = { typeof(int) };
-        ConstructorBuilder ctor = tb.DefineConstructor(
-            MethodAttributes.Public, 
-            CallingConventions.Standard, 
-            parameterTypes);
-        
-        ILGenerator il = ctor.GetILGenerator();
-        il.Emit(OpCodes.Ldarg_0);
-        il.Emit(OpCodes.Ldarg_1);
-        il.Emit(OpCodes.Stfld, fbNumber);
-        il.Emit(OpCodes.Ret);
-        
-        // Overrides ToString() method
-        MethodBuilder toStr = tb.DefineMethod("ToString",
-             MethodAttributes.Public | MethodAttributes.ReuseSlot | 
-             MethodAttributes.Virtual | MethodAttributes.HideBySig,
-            typeof(string), 
-            new Type[0]);
-            
-        MethodInfo concat = typeof(string)
-            .GetMethod("Concat", new Type[]{typeof(object), typeof(object)});
-        il = toStr.GetILGenerator();
-        il.Emit(OpCodes.Ldstr, "m_number = ");
-        il.Emit(OpCodes.Ldarg_0);
-        il.Emit(OpCodes.Ldfld, fbNumber);
-        il.Emit(OpCodes.Box, fbNumber.FieldType);
-        il.Emit(OpCodes.Call, concat);
-        il.Emit(OpCodes.Ret);
-
-        // Finish the type.
-        Type t = tb.CreateType();
-
-        // The following line saves the single-module assembly. This
-        // requires AssemblyBuilderAccess to include Save. 
-        // 
-        ab.Save(aName.Name + ".dll");
-        return t;
-            */
         }
 
         private static MethodBuilder buildMethod(TypeBuilder tb, string methodName, Type retType, Type[] paramsType)
@@ -141,9 +80,28 @@ namespace SqlReflect
             paramsType);
         }
 
-        private static void buildLoadMethod()
+        private static void BuildLoadMethod(TypeBuilder tb, string methodName, Type klass)
         {
-            throw new NotImplementedException();
+            Type[] parametersType = new Type[] { typeof(IDataReader) };
+            MethodBuilder mbLoadMethod = buildMethod(tb, "Load", klass, parametersType);
+
+            ILGenerator il = mbLoadMethod.GetILGenerator();
+
+            il.Emit(OpCodes.Nop);
+            il.Emit(OpCodes.Ldloca_S);
+            il.Emit(OpCodes.Initobj);
+
+            foreach (PropertyInfo p in klass.GetProperties())
+            {
+                il.Emit(OpCodes.Ldloca_S);
+                il.Emit(OpCodes.Ldarg_1);
+                il.Emit(OpCodes.Ldstr, p.PropertyType);
+                il.Emit(OpCodes.Callvirt, p.GetGetMethod());
+                il.Emit(OpCodes.Castclass, klass);
+                il.Emit(OpCodes.Call, );
+            }
+            il.Emit(OpCodes.Ret);
+
         }
 
     }
